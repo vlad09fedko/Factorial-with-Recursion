fe8890ac1bdc260e08238952cc269674984b5d1f
Vlad <vlad09fedko@gmaul.com>
2026-04-21 20:00:52 +0300

    Do app

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 'use strict';
+
+function countFactorial(num = 1){
+  if(typeof num !== 'number' || Number.isNaN(num)) return 'Error!'
+  if (num === 1) return num;
+  return num * countFactorial(num - 1)
+}
+
+console.log(countFactorial(10))
