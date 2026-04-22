71da5d2382b8d0d1b0732e8153a0b55ed5aa3bc3
Vlad <vlad09fedko@gmaul.com>
2026-04-22 13:31:30 +0300

    Clear code

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@
 
 function countFactorial(num = 1) {
   if (typeof num !== 'number' || Number.isNaN(num) || num < 0) return 'Error!';
-  if (num === 0) return 1;
-  if (num === 1) return num;
+  if (num === 0 || num === 1) return 1;
   return num * countFactorial(num - 1);
 }
 
