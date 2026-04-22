1fbd7013f21a400e1b8555f483118fe602ae7ae4
Vlad <vlad09fedko@gmaul.com>
2026-04-22 13:03:24 +0300

    Fix negative num bug

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 'use strict';
 
 function countFactorial(num = 1) {
-  if (typeof num !== 'number' || Number.isNaN(num)) return 'Error!';
+  if (typeof num !== 'number' || Number.isNaN(num) || num < 0) return 'Error!';
+  if (num === 0) return 1;
   if (num === 1) return num;
   return num * countFactorial(num - 1);
 }
 
-console.log(countFactorial(10));
+console.log(countFactorial(5));
